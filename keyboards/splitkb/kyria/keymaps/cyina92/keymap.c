0d0c63b9cd879823991e391cf7b8d9df2c2435e0
Timo Kersting <timokersting@e.mail.de>
2021-10-23 17:03:22 +0200

    oled layout for Vou

--- keyboards/splitkb/kyria/keymaps/cyina92/keymap.c
+++ keyboards/splitkb/kyria/keymaps/cyina92/keymap.c
@@ -245,7 +245,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * DO NOT edit the rev1.c file; instead override the weakly defined default functions by your own.
  */
 
-/* DELETE THIS LINE TO UNCOMMENT (1/2)
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_180; }
 
@@ -265,6 +264,9 @@ void oled_task_user(void) {
         // Host Keyboard Layer Status
         oled_write_P(PSTR("Layer: "), false);
         switch (get_highest_layer(layer_state|default_layer_state)) {
+            case _VOU:
+                oled_write_P(PSTR("Vou\n"), false);
+                break;
             case _QWERTY:
                 oled_write_P(PSTR("QWERTY\n"), false);
                 break;
@@ -334,4 +336,3 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     return false;
 }
 #endif
-DELETE THIS LINE TO UNCOMMENT (2/2) */
